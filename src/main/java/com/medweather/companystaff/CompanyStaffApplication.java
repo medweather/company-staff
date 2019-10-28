b9490abf7a834d5e29163d93e5703f8c1c32da80
Alexandr Medvedev <medweather@mail.ru>
2019-10-28 23:51:35 +0300

    initial project

---
+++ src/main/java/com/medweather/companystaff/CompanyStaffApplication.java
@@ -0,0 +1,13 @@
+package com.medweather.companystaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyStaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyStaffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medweather/companystaff/CompanyStaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medweather.companystaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyStaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
